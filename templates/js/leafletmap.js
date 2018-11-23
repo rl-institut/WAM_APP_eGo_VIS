c03d3e754a85f665a53e4ccece0d86ae5fd221b0
jh-RLI <Jonas.Huber@rl-institut.de>
2018-11-23 15:13:37 +0100

    "add template folder"

---
+++ templates/js/leafletmap.js
@@ -0,0 +1,7 @@
+<script src='js/leafletmap.js'></script>
+
+<script type="text/javascript">
+    function map_init_basic (map, options) {
+        L.marker([50.5, 30.5]).addTo(map);
+    }
+    </script>
